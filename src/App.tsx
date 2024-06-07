4afeec2b4a2e438f526778d4bfcb22c0f9f83b2a
Stephan <stephan_habermacher@gmx.ch>
2024-06-07 20:52:18 +0200

    rootDir

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import LoginView from "./views/LoginView";
-import OuterenvelopeView from "./views/ProductView";
-import ProjectView from "./views/ProjectView";
 import ProjectsOverviewView from "./views/ProjectsOverviewView";
 
 function App() {
